f05ea9941a80f28f066c15729bd8742ab0920f4f
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-12 19:25:57 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer 
   attr_accessor :name 
   
-  @@project_array = []
+  @project_array = []
   
 def initialize(name)
   @name = name 
 end
 
 def back_project(project)
-  @@project_array << project
+  @project_array << project
 end 
 
 end 
