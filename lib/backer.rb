fa9af8435c817af65f8ecb8d9894093982c51095
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-12 19:28:08 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,11 +5,11 @@ class Backer
   
 def initialize(name)
   @name = name 
-  @backed_project = []
+  @backed_projects = []
 end
 
 def back_project(project)
- @backed_project << project
+ @backed_projects << project
 end 
 
 end 
