35362592365c0438432d1e04e2d0767a2e7ca9db
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-12 19:30:30 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,11 @@
 class Project 
-  attr_accessor :title 
+  attr_accessor :title, :backers_array 
   
 def initialize(title)
   @title = title
+  @backers_array = []
 end
+
+def add_badker(backer)
+
 end 
